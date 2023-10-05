f4fc36fc4a3a11cb4deb9e9a2ab99a84a713cd28
quakhubuon <tranminh2014.tm@gmail.com>
2023-10-05 11:26:40 +0700

    first commit

---
+++ app/Models/Permission.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Permission extends Model
+{
+    public $timestamps = false;
+
+    protected $fillable = [
+        'name',
+        'guard_name',
+    ];
+}
---
+++ app/Models/Role.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    public $timestamps = false;
+
+    protected $fillable = [
+        'name',
+        'guard_name',
+    ];
+}
